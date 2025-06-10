cef618e0c779376dc05514b0a2841b9c963f39a8
KevinPrada <santicastellanos2017@gmail.com>
2025-06-10 10:52:19 -0500

    Agregando atributos a la clase Copia

--- source/uniandes/cupi2/videotienda/mundo/Copia.java
+++ source/uniandes/cupi2/videotienda/mundo/Copia.java
@@ -1,5 +1,66 @@
 package uniandes.cupi2.videotienda.mundo;
 
+
+/**
+ * Esta clase representa una copia de la película que se encuentra en la videotienda 
+ */ 
 public class Copia {
+	
+	 //-----------------------------------------------------------------
+    // Atributos
+    //-----------------------------------------------------------------
+
+    /**
+     * Código de la copia
+     */
+	private int codigo;
+	
+	/**
+	 * Titulo de la copia de la película
+	 */
+	private String tituloPelicula;
+	
+	
+	//-----------------------------------------------------------------
+    // Constructores
+    //-----------------------------------------------------------------
+
+    /**
+     * Crea una nueva copia de una película.
+     * @param elCodigo Código único de la copia. elCodigo > 0.
+     * @param laPelicula Título de la película. laPelicula != null.
+     */
+	public Copia( int elCodigo, String laPelicula) {
+		codigo = elCodigo;
+		tituloPelicula = laPelicula;
+	}
+	
+	/**
+	 * Retorna el código de la copia.
+	 * @return Código de la copia
+	 */
+	public int darCodigo() {
+		return codigo;
+	}
+	
+	/**
+	 * Retorna el título de la pélicula.
+	 * @return Título de la película.
+	 */
+	public String darTituloPelicula() {
+		
+		return tituloPelicula;
+	}
+	
+    /**
+     * Compara esta copia con otra para saber si son iguales (mismo código y misma película).
+     * @param otra La otra copia a comparar. otra != null.
+     * @return true si ambas copias son iguales, false en caso contrario.
+     */
+	public boolean esIgualA(Copia otra) {
+		return otra != null && codigo == otra.darCodigo() &&
+				tituloPelicula.equals(otra.darTituloPelicula());
+	}
+	
 
 }
